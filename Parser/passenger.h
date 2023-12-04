7df7f00bb4eb047eca59b4ccb0846731667b31bf
Shreyan Bagchi <bagchishreyan@gmail.com>
2023-12-04 23:17:38 +0530

    Update passenger.h
    Enums and functions redefined

--- Parser/passenger.h
+++ Parser/passenger.h
@@ -1,37 +1,37 @@
 #include<bits/stdc++.h>
-enum SpecialNames1 {INS,INF,CHD,ADT,UNN,S65};
-enum SpecialNames2 {NRPS,NRSA};
-enum SSRCodes {WCHR,WCHS,WCHC,LANG,MAAS,UNMR,BLND,DEAF,EXST,MEAL,NSST};
+enum SpecialNames1 {NIL1,INS,INF,CHD,ADT,UNN,S65};
+enum SpecialNames2 {NIL2,NRPS,NRSA};
+enum SSRCodes {NIL3,WCHR,WCHS,WCHC,LANG,MAAS,UNMR,BLND,DEAF,EXST,MEAL,NSST};
 
 int getSN1Code(string str){
-    if(str=="INS") return 0;
-    else if(str=="INF") return 1;
-    else if(str=="CHD") return 2;
-    else if(str=="ADT") return 3;
-    else if(str=="UNN") return 4;
-    else if(str=="S65") return 5;
-    else return -1;
+    if(str=="INS") return 1;
+    else if(str=="INF") return 2;
+    else if(str=="CHD") return 3;
+    else if(str=="ADT") return 4;
+    else if(str=="UNN") return 5;
+    else if(str=="S65") return 6;
+    else return 0;
 }
 
 int getSN2Code(string str){
-    if(str=="NRPS") return 0;
-    else if(str=="NRSA") return 1;
-    else return -1;
+    if(str=="NRPS") return 1;
+    else if(str=="NRSA") return 2;
+    else return 0;
 }
 
 int getSSRCode(string str){
-    if(str=="WCHR") return 0;
-    else if(str=="WCHS") return 1;
-    else if(str=="WCHC") return 2;
-    else if(str=="LANG") return 3;
-    else if(str=="MAAS") return 4;
-    else if(str=="UNMR") return 5;
-    else if(str=="BLND") return 6;
-    else if(str=="DEAF") return 7;
-    else if(str=="EXST") return 8;
-    else if(str=="MEAL") return 9;
-    else if(str=="NSST") return 10;
-    else return -1;
+    if(str=="WCHR") return 1;
+    else if(str=="WCHS") return 2;
+    else if(str=="WCHC") return 3;
+    else if(str=="LANG") return 4;
+    else if(str=="MAAS") return 5;
+    else if(str=="UNMR") return 6;
+    else if(str=="BLND") return 7;
+    else if(str=="DEAF") return 8;
+    else if(str=="EXST") return 9;
+    else if(str=="MEAL") return 10;
+    else if(str=="NSST") return 11;
+    else return 0;
 }
 
 class Passenger{
